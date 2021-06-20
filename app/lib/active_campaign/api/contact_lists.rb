d19b07289872dee2a30e20928ca78567eb07909b
Udit Mittal <uditmittal@Udits-MacBook-Pro.local>
2021-06-21 03:37:54 +0530

    Fixes contact_lists for production

--- app/lib/active_campaign/api/contact_lists.rb
+++ app/lib/active_campaign/api/contact_lists.rb
@@ -5,19 +5,26 @@
 #
 # @author Udit Mittal <udit.mittal1990@gmail.com>
 #
-module ActiveCampaign::API::ContactLists
+ActiveCampaign::API.module_eval do
   #
-  # Add a list to a contact
+  # Interface to contact tag endpoints
   #
-  # @param [Hash] params add a list to a contact with this data
-  # @param params [String] :contact ID of the contact you're adding the tag to
-  # @param params [String] :list ID of the list to be added for the contact
-  # @param params [String] :status Set to "1" to subscribe the contact to the list. Set to "2" to unsubscribe the contact from the list.
-  # @param params [String] :sourceid Set to "4" when re-subscribing a contact to a list
+  # @author Udit Mittal <udit.mittal1990@gmail.com>
   #
-  # @return [Hash] a hash with the information of the newly created contact list
-  #
-  def create_contact_list(params)
-    post('contactLists', contact_list: params)
+  module ContactLists
+    #
+    # Add a list to a contact
+    #
+    # @param [Hash] params add a list to a contact with this data
+    # @param params [String] :contact ID of the contact you're adding the tag to
+    # @param params [String] :list ID of the list to be added for the contact
+    # @param params [String] :status Set to "1" to subscribe the contact to the list. Set to "2" to unsubscribe the contact from the list.
+    # @param params [String] :sourceid Set to "4" when re-subscribing a contact to a list
+    #
+    # @return [Hash] a hash with the information of the newly created contact list
+    #
+    def create_contact_list(params)
+      post('contactLists', contact_list: params)
+    end
   end
 end
