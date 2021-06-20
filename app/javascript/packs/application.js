81e26d58716bf0c568291fa3f7b62b1ce1dfac05
Udit Mittal <uditmittal@Udits-MacBook-Pro.local>
2021-06-21 04:59:18 +0530

    Adds filteration form in tasks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,7 +14,13 @@ require("@rails/activestorage").start()
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-var elem = document.querySelector('.datepicker');
-var datepicker = new Datepicker(elem, {
-  // ...options
-}); 
+window.addEventListener('load', function() {
+  var datepickers = document.querySelectorAll('.db-datepicker');
+  var i;
+
+  for (i = 0; i < datepickers.length; i++) {
+    var datepicker = new Datepicker(datepickers[i], {
+      format: 'mm-dd-yyyy'
+    });
+  }
+})
